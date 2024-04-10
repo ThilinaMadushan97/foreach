f864e8a7106f8518adb6d22d83589f4585d3aa0d
dell <you@example.com>
2024-04-11 00:05:34 +0530

    Add project files.

---
+++ Course2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] names = new string[3];
+
+names[0] = "A123";
+names[1] = "B123";
+names[2] = "C123";
+names[3] = "D123";
+
+
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+foreach(string na in names)
+{
+    Console.WriteLine($"Your names is {na}");
+}
